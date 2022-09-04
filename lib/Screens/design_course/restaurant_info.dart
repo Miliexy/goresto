34752c9f110a2dd7060b185344193fc239c604de
Ilyasse Lahrach <masterox@outlook.com>
2022-09-04 15:13:35 +0100

    - Big updated - UI redesign - Map redesign

--- lib/Screens/design_course/restaurant_info.dart
+++ lib/Screens/design_course/restaurant_info.dart
@@ -56,7 +56,7 @@ class _CourseInfoScreenState extends State<CourseInfoScreen>
               children: <Widget>[
                 AspectRatio(
                   aspectRatio: 1.2,
-                  child: Image.asset('assets/design_course/webInterFace.png'),
+                  child: Image.asset('assets/images/fine-dining1.jpg'),
                 ),
               ],
             ),
